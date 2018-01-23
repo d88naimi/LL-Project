3ecf6f2bcd5e6046306a216ecb9bdc56a317a015
David Naimi <d88naimi@gmail.com>
2018-01-23 13:04:10 -0800

    small bug with jquery

--- app.js
+++ app.js
@@ -17,19 +17,30 @@ var images = {
 };
 
 
-$(document).ready(function () {
-for (var j in images) {
-    console.log(images[j]);
-    console.log(JSON.stringify((images[j])));
-    console.log(JSON.stringify((images[j].heading)));
-    console.log("------------");
-    console.log(((images[j].path)));
+// $(function () {
+// for (var j in images) {
+//     console.log(images[j]);
+//     console.log(JSON.stringify((images[j])));
+//     console.log(JSON.stringify((images[j].heading)));
+//     console.log("------------");
+//     console.log(((images[j].path)));
     
 
 
-}
+//     var image = $("<div>");
+//     var col = $("<div>").addClass("col-xs-12 col-sm-4 col-md-4 col-lg-4");
+//     var text = $("<div>").addClass("text");
+//     image.append(text);
+//     col.append(image);
+//     console.log("------------");
+//     $("#images").append(col);
+//     image.css("background-image", 'url("' + images[j].path + '")');
+//     image.addClass("imageContainer");
+//     image.addClass("image");
+//     image.addClass("overlay");
+// }
 
-});
+// });
 
 // .imageContainer
 
@@ -41,8 +52,6 @@ for (var j in images) {
 
 
 // findInObj(images, "heading", "description");
-
-
 
 
 // $(document).ready(function () {
